6a3c5094dd1c3e689cbdf34f54d4c81ad62effe9
alexander-jimenez11 <gajimenez2@utpl.edu.ec>
2021-05-19 18:59:15 -0500

    Tarea

--- EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/ArchivoLectura.java
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/ArchivoLectura.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete6;
 
 import java.io.File;
@@ -11,10 +6,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- *
- * @author Alexander
- */
 public class ArchivoLectura {
 
     private Scanner entrada;
--- EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Hospital.java
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Hospital.java
@@ -43,30 +43,10 @@ public class Hospital {
     @Override
     public String toString() {
         String cadena = "";
-        cadena = String.format("HOSPITAL %s\nDirección: %s\nCiudad: %s"
-                + "\nProvincia: %s\nNúmero de especialidades: %d"
-                + "\nListado de médicos\n", obtenerNombre(),
-                obtenerDireccionHospital(),
-                obtenerCiudadHospital(), obtenerProvinciaHospital(),
-                obtenerNumeroEspecialidades());
-        for (int i = 0; i < obtenerMedicos().length; i++) {
-            cadena = String.format("%s- %s - sueldo: %.2f - %s\n", cadena,
-                    obtenerMedicos()[i].obtenerNombre(),
-                    obtenerMedicos()[i].obtenerSueldo(),
-                    obtenerMedicos()[i].obtenerEspecialidad());
-        }
-        cadena = String.format("%s\nListado de enfermeros(as)\n", cadena);
-        for (int i = 0; i < obtenerEnfermeros().length; i++) {
-            cadena = String.format("%s - %s - sueldo: %.2f - %s\n", cadena,
-                    obtenerEnfermeros()[i].obtenerNombre(),
-                    obtenerEnfermeros()[i].obtenerSueldo(),
-                    obtenerEnfermeros()[i].obtenerEspecialidad());
-        }
-        cadena = String.format("%s \nTotal de sueldos a pagar por mes: %.2f",
-                cadena, obtenerTotalSueldo(obtenerMedicos(),
-                        obtenerEnfermeros()));
+        cadena = String.format("HOSPITAL %s\nNombre: %s\nNumero de camas: %d\n"
+                + "Presupuesto %.2f\n",
+                obtenerNombre(), obtenerNumeroCamas(), obtenerPresupuesto());
+
         return cadena;
     }
-}
-
 }
